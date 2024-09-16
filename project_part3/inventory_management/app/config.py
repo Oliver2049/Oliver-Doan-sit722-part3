1c3f32b3c14c587597c0c30e6a2ed06dec9408dd
Oliver <minhtien2049@gmail.com>
2024-09-17 00:11:14 +1000

    First commit nventory-management:1

--- project_part3/inventory_management/app/config.py
+++ project_part3/inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://oliver_722_part3_user:aoN2388BKPqP3JABpQYThRzot8FoYSN4@dpg-criodclumphs73cnp5cg-a.oregon-postgres.render.com/oliver_722_part3')
